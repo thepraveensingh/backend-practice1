481fa4eec857d6e163ea44f1b510c1a697cca473
thepraveensingh <praveensingh007rawat@gmail.com>
2024-09-28 22:21:14 +0530

    readmore implemented

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/', (req, res) => {
     })
     
 });
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render("show",{filename : req.params.filename, filedata : filedata});
+    })
+    
+});
 app.post('/create', (req, res) => {
         
         fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err) => {
